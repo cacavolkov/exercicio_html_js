1fd1f0e7e09a762121d665c7ffb6a688727b6ef9
Carlos Alberto <bravo_c@hotmail.com>
2023-10-04 12:17:23 -0300

    correção

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
-
 document.getElementById("myForm").addEventListener("submit", function(event){
-    event.preventDefault()
-
-    var campoA = document.getElementById("campoA").value;
-    var campoB = document.getElementById("campoB").value;
-
-    if (campoB > campoA) {
-    document.getElementById("message").innerHTML = "Formulário válido!";
-    document.getElementById("message").style.color = "green";
-    document.getElementById("message").style.display = "block";
-
-    return false;
+    event.preventDefault();
     
+    var campoA = parseFloat(document.getElementById("campoA").value);
+    var campoB = parseFloat(document.getElementById("campoB").value);
+    
+    if (campoB > campoA) {
+        document.getElementById("message").innerHTML = "Formulário válido!";
+        document.getElementById("message").style.color = "green";
     } else {
-    document.getElementById("message").innerHTML = "Formulário inválido!";
-    document.getElementById("message").style.color = "red";
-    document.getElementById("message").style.display = "block";
-
-    return false;
-
+        document.getElementById("message").innerHTML = "Formulário inválido!";
+        document.getElementById("message").style.color = "red";
     }
 });
